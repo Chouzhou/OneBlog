b97cb99b633656022e1e124209365cf7ae3a68f5
Chouzhou <z846204062@hotmail.com>
2017-03-09 23:51:52 +0800

    修改一些小数据

--- OneBlog/src/pages/home.vue
+++ OneBlog/src/pages/home.vue
@@ -1,10 +1,11 @@
 <template>
   <div class="container">
+    <!--头部信息-->
     <div class="header">
       <el-menu theme="dark" :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-        <el-menu-item index="1">首页</el-menu-item>
-        <el-menu-item index="3">写文章</a></el-menu-item>
-        <el-submenu index="2" class="myInfo"> 
+        <!--<el-menu-item index="1">首页</el-menu-item>
+        <el-menu-item index="3">写文章</a></el-menu-item>-->
+        <el-submenu index="2" class="myInfo" style="text-align: right;"> 
           <template slot="title">{{ name }}</template>
           <el-menu-item index="2-1">个人资料</el-menu-item>
           <el-menu-item index="2-2">所写文章</el-menu-item>
@@ -13,12 +14,13 @@
       </el-menu>
     </div>
     <div class="line"></div>
+    <!--侧边栏-->
     <div class="leftmenu"> 
       <el-row class="tac">
         <el-col :span="30">         
           <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" theme="dark">
             <el-submenu index="1">
-              <template slot="title"><i class="el-icon-message"></i>导航一</template>
+              <template slot="title"><i class="el-icon-home"></i>首頁</template>
               <el-menu-item-group>
                 <template slot="title">分组一</template>
                 <el-menu-item index="1-1">选项1</el-menu-item>
@@ -28,15 +30,25 @@
                 <el-menu-item index="1-3">选项3</el-menu-item>
               </el-menu-item-group>
               <el-submenu index="1-4">
-                <template slot="title">选项4</template>
+                <template slot="title">写文章</template>
                 <el-menu-item index="1-4-1">选项1</el-menu-item>
               </el-submenu>
             </el-submenu>
-            <el-menu-item index="2"><i class="el-icon-menu"></i>导航二</el-menu-item>
-            <el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>
+            <el-menu-item index="2"><i class="el-icon-edit"></i>首頁</el-menu-item>
+            <el-menu-item index="2"><i class="el-icon-edit"></i>写文章</el-menu-item>
+            <!--<el-menu-item index="3"><i class="el-icon-setting"></i>导航三</el-menu-item>-->
           </el-menu>
         </el-col>       
       </el-row>
+    </div>
+    <!--显示首页信息-->
+    <div class="main">
+      <el-breadcrumb separator="/">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+        <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+        <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+      </el-breadcrumb>
     </div>
   </div>
 </template>
@@ -63,6 +75,9 @@ export default {
   .header{
     position:relative;
   }
+  .myInfo {
+      text-align: right;
+  }
   .leftmenu{
     background: #324057;
 		position: absolute;
@@ -73,5 +88,12 @@ export default {
     aside {
 				width: 230px;
 			}
+  }
+  /*主体部分*/
+  .main {
+    position: relative;
+    top: 10px;
+    left: 205px;
+
   }
 </style>
